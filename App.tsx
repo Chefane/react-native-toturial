b6ded3bae48134bc6635a6ab7567533ba85b1d1c
Nosi Chefane <nosichefane@mail.com>
2022-12-17 18:38:54 +0200

    added the navigation bar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { AntDesign } from '@expo/vector-icons';
+import { createMetaProperty } from "typescript";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.navigation}>
+        <Text style={styles.textStyle}>Properties</Text>
+  
+          <Text style={styles.icons}>
+            <Ionicons name="notifications-outline" size={20} color="black" />
+          </Text>
+          <Text style={styles.antDesign}>
+          <AntDesign name="hearto" size={20} color="black" />
+          </Text>
+            
+        
+        
+      </View>
+      <View style={styles.body}>
+        <Text>Body</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +31,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+
   },
+
+  navigation: {
+    flex: 2,
+    marginTop: 60,
+    marginLeft: 20,
+    color: "white",
+    width: "100%",
+  },
+
+  textStyle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+  icons: {
+  textAlign: 'right',
+  marginRight: 40,
+  marginTop: -30
+    
+  },
+
+  antDesign: {
+  textAlign: 'right',
+  marginRight: 70,
+  marginTop: -25
+  },
+
+  body: {
+    flex: 20,
+    alignItems: "center",
+  }
 });
